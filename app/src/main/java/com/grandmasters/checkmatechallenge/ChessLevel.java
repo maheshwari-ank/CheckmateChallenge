f422a03b8ded3be34c419a5e0843302d7af69024
Ankush Maheshwari <maheshwari.ank@northeastern.edu>
2024-04-17 12:06:22 -0400

    added stack implementation

--- app/src/main/java/com/grandmasters/checkmatechallenge/ChessLevel.java
+++ app/src/main/java/com/grandmasters/checkmatechallenge/ChessLevel.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.Stack;
+//import java.util.Stack;
 import java.io.Serializable;
 
 public class ChessLevel implements ChessDelegate, Serializable {
---
+++ app/src/main/java/com/grandmasters/checkmatechallenge/Stack.java
@@ -0,0 +1,61 @@
+package com.grandmasters.checkmatechallenge;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class Stack<E> {
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<E> top;
+    private int size;
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    public void push(E element) {
+        Node<E> newNode = new Node<>(element);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public E pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        E poppedData = top.data;
+        top = top.next;
+        size--;
+        return poppedData;
+    }
+
+    public E peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void removeAllElements() {
+        top = null;
+        size = 0;
+    }
+}
